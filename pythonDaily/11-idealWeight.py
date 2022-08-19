b62c08285494b570afa9f4ecf49a037c8c413af7
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-19 15:02:42 -0300

    new functionality: man/weman specific math function

--- pythonDaily/11-idealWeight.py
+++ pythonDaily/11-idealWeight.py
@@ -6,6 +6,22 @@ print()
 print(">>> Ideal weight calculator <<<")
 print()
 
+
+# Asking gender
+
+gender = ""
+
+while True:
+        
+        # Gender normalization
+    gender = input("Are you a Man or a Woman? (M / W): ")
+    gender = gender[0].rstrip().lower()
+
+    if gender == "m" or gender == "w":
+        break
+    else:
+        print("Invalid entry. Try again!")
+
 while True:
     height = input("Insert your height (cm): ")    
     try:
@@ -14,9 +30,21 @@ while True:
     except ValueError:
         print("Error. Insert a number!")
 
+    # height normalization
 height = height/100
 
-idealWeight = (72.7 * height) - 58
+    # Ideal weight math
+
+idealWeight = 0
+
+        # Man math
+if gender == "m":
+    idealWeight = (72.7 * height) - 58
+
+        # Woman math
+elif gender == "w":
+    idealWeight = (62.1 * height) - 44.7
+
 
 print()
 print("This is your ideal weight:")
