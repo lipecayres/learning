95eb53855988a18144605ece84d468014968c750
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-19 16:05:15 -0300

    function to validate number entry

--- pythonDaily/13-salary.py
+++ pythonDaily/13-salary.py
@@ -6,11 +6,28 @@
 ### CODE ###
 
 def start():
+    intro()
 
 
+    # Introduction Layout
 def intro():
     print()
-    print(">>> Salary Calculator <<<")
+    print("### Salary Calculator ###")
     print()
+
+
+    # function to validate number entry
+def numberEntry():
+    while True:
+        n = input("Value: ")    
+        try:
+            n = float(n)
+            break
+        except ValueError:
+            print("Invalid entry. Try again!")
+    return n
+
+
+
 
 start()
