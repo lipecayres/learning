30613bb0bb43ecdb6513251b7484d74ca5c9d7f7
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-19 15:48:37 -0300

    Start Project Salary

---
+++ pythonDaily/13-salary.py
@@ -0,0 +1,6 @@
+# System to calculate discounts on a employee salary.
+# Input: Salary/hour  | WOrking hours (month)
+# Return: Final Salary  after all discounts.
+# Discounts: IR = 11% | INSS = 8% | syndicate: 5%
+
+### CODE ###
