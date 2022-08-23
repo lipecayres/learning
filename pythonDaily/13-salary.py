a9435895ba64bd06422766d807bf32ced725c791
lipecayres <103278875+lipecayres@users.noreply.github.com>
2022-08-23 16:36:07 -0300

    add payment function - salary math

--- pythonDaily/13-salary.py
+++ pythonDaily/13-salary.py
@@ -5,8 +5,42 @@
 
 ### CODE ###
 
+
+#Variables
+totalSalary = 0
+ir = 0
+inss = 0
+syndicate = 0
+finalSalary = 0
+
+
 def start():
     intro()
+    payment()
+  #  summary()
+
+
+
+def payment ():
+    global totalSalary 
+    global finalSalary 
+    global ir 
+    global inss 
+    global syndicate
+    
+    print("Hou much money do you make per hour?")
+    moneyHour = floatEntry()
+
+    print("Hou many hours do you hork per month?")
+    workTime = floatEntry()
+
+    totalSalary = moneyHour * workTime
+    ir = totalSalary * 0.11
+    inss = totalSalary * 0.08
+    syndicate = totalSalary *0.05
+    finalSalary = totalSalary - ir - inss - syndicate
+
+    print(f" {totalSalary} , {finalSalary}, {ir}, {inss}, {syndicate}")
 
 
     # Introduction Layout
