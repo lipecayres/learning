b3efd98b0eb5578741375985c1767457b9e626a2
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-11-17 11:27:34 -0500

    ex3 done

---
+++ fullstack/m1/variables/ex3.js
@@ -0,0 +1,7 @@
+let x = 10, y = 2;
+
+
+console.log(`Sum of ${x} + ${y} is: ${x+y}`);
+console.log(`Subtraction of ${x} - ${y} is: ${x-y}`);
+console.log(`Product of ${x} x ${y} is: ${x*y}`);
+console.log(`Division of ${x} / ${y} is: ${x/y}`);
