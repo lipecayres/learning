48691ff15f06535a07e60e185298e3fff62af3b6
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-11-26 11:48:41 -0500

    cilinder area ex

---
+++ fullstack/m1/variables/cilinderArea.js
@@ -0,0 +1,11 @@
+let totalArea, baseArea, rectangleArea;
+
+let radius = 2, height = 3;
+
+
+baseArea = Math.PI *radius**2
+rectangleArea =  2 * Math.PI * radius * height 
+
+totalArea = 2*baseArea + rectangleArea;
+
+console.log(`total area is ${totalArea}m²`)
