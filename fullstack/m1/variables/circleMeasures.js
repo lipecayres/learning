72fbc21beee0cc67a48546531ae565c43a904f64
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-11-26 11:20:29 -0500

    circle circunference ok

---
+++ fullstack/m1/variables/circleMeasures.js
@@ -0,0 +1,6 @@
+let circunference, radius = 2, area;
+
+circunference = 2 * Math.PI * radius;
+area = Math.PI * (radius**2);
+
+console.log(`Circle area is ${area} and perimeter is ${circunference}`);
