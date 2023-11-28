5bb20e4e810f568262338b2984d6971690e9d958
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-11-27 20:41:22 -0500

    new condition "or"

--- fullstack/m1/conditions/sample1.js
+++ fullstack/m1/conditions/sample1.js
@@ -1,7 +1,8 @@
-const hasTicket = true;
-const ageLimit = 16, age = 28;
+const hasTicket = true, parentsWith = false;
+const ageLimit = 16, age = 8;
 
-if (hasTicket && age >= ageLimit) {
+
+if (hasTicket && (age >= ageLimit || parentsWith == true)) {
     console.log("Access OK");
 } else {
     console.log("Access NOT OK");
