41f23374cb0b027df82652f8634c42734fead612
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-11-27 20:54:31 -0500

    comparisson clean code

--- fullstack/m1/conditions/sample1.js
+++ fullstack/m1/conditions/sample1.js
@@ -2,7 +2,7 @@ const hasTicket = true, parentsWith = false;
 const ageLimit = 16, age = 8;
 
 
-if (hasTicket && (age >= ageLimit || parentsWith == true)) {
+if (hasTicket && (age >= ageLimit || parentsWith)) {
     console.log("Access OK");
 } else {
     console.log("Access NOT OK");
