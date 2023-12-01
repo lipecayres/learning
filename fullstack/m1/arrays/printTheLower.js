de807805aa562162393f5089da17a55e6aa20184
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-12-01 00:48:26 -0500

    print the lower ok

---
+++ fullstack/m1/arrays/printTheLower.js
@@ -0,0 +1,10 @@
+const arr1 = [5,32,3,44,1];
+const arr2 = [57,4,21,2,13];
+
+for(let i = 0; i< arr1.length; i++){
+    if (arr1[i] < arr2[i]) {
+        console.log(arr1[i])
+    } else {
+        console.log(arr2[i])
+    }
+}
