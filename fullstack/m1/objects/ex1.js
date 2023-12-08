23efc14eacf26eda59061f37c722975d29334d5d
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2023-12-08 00:50:07 -0500

    ex1 done

---
+++ fullstack/m1/objects/ex1.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'Felipe',
+    age: 30,
+    height: 184,
+    hasDriverLicence: true,
+    nicknames: ['Lipe', 'Fe']
+}
+
+driverLicenseCondition = (person.hasDriverLicence == true ? "has DL" : "don't have DL");
+
+console.log(
+    `"${person.name} has ${person.age} years, ${person.height}cm height, ${driverLicenseCondition} and these nicknames: 
+    - ${person['nicknames'][0]}
+    - ${person['nicknames'][1]}"`)
