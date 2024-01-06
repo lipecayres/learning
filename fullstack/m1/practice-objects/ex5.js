00846a28c716d11e640e65a06cb7fc9e0d09c3c8
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2024-01-06 08:16:54 -0500

    ex5.js

---
+++ fullstack/m1/practice-objects/ex5.js
@@ -0,0 +1,24 @@
+const course = {
+    id: 1234,
+    name: 'Programming Logic',
+    classes: [
+        {
+            identifier: 1,
+            className: 'Introduction to Programming'
+        },
+        {
+            identifier: 2,
+            className: 'Variables'
+        },
+        {
+            identifier: 3,
+            className: 'Conditions'
+        },
+        {
+            identifier: 4,
+            className: 'Arrays'
+        }
+    ]
+}
+
+console.log(course);
