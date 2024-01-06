d6e268bfb5d9b68990567f138ff1286cc89fcceb
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2024-01-06 07:57:16 -0500

    ex2 done

---
+++ fullstack/m1/practice-objects/ex2.js
@@ -0,0 +1,26 @@
+const cars = [
+    {
+        brand: 'Ford',
+        model: 'Ka',
+        year: 2020,
+        color: 'Red',
+        doorsAmount: 4,
+        automatic: false
+    },
+    {
+        brand: 'Toyota',
+        model: 'Corola',
+        year: 2018,
+        color: 'Black',
+        doorsAmount: 4,
+        automatic: true
+    },
+    {
+        brand: 'Fiat',
+        model: 'Uno',
+        year: 2013,
+        color: 'White',
+        doorsAmount: 2,
+        automatic: false
+    }
+];
