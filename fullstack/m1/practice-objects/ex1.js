2fa6f2273d7dbfbbdd0e71d8b0742f75f8097637
Felipe Cayres <103278875+lipecayres@users.noreply.github.com>
2024-01-06 07:57:11 -0500

    ex1 done

---
+++ fullstack/m1/practice-objects/ex1.js
@@ -0,0 +1,21 @@
+const address = {
+    street: 'Edward St',
+    number: 20,
+    comp: 'ap 12',
+    neighborhood: 'downtown',
+    zipcode: 'M1M2M3',
+    city: 'Toronto',
+    province: 'Ontario',
+    country: 'Canada'
+}
+
+const user = {
+    name: 'Felipe',
+    email: 'felipe@gmail.com',
+    phoneNumber: '4564574589',
+    birthday: '10/15/1993',
+    address
+
+}
+
+console.log(user);
